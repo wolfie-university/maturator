8e8bc2e24f6de75b4d33d1d5f7cd4b4e8f05184f
Szymon Wilczek <szymonwilczek@outlook.com>
2025-12-05 22:17:28 +0100

    feat: link to the project in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 export default function Footer() {
   return (
     <footer className="w-full py-4 text-center text-foreground/20 text-sm border-t bg-background">
-      <p>© 2025 Szymon Wilczek <br /> Maturator - Projekt Open Source</p>
+      <p>© 2025 Szymon Wilczek <br />
+        <Button variant="link" asChild
+          className="p-0 m-0 align-baseline text-foreground/20 hover:underline transition-opacity hover:opacity-80"
+        >
+          <Link
+            href="https://github.com/wolfie-university/maturator"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maturator - Projekt Open Source
+          </Link>
+        </Button>
+      </p>
     </footer>
   );
 }
