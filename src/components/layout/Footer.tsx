955839689b87c1db18f581f6ab589b86d0adff15
Szymon Wilczek <szymonwilczek@outlook.com>
2025-12-05 04:08:49 +0100

    feat: new footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-4 text-center text-foreground/20 text-sm border-t bg-background">
+      <p>© 2025 Szymon Wilczek <br /> Maturator - Projekt Open Source</p>
+    </footer>
+  );
+}
